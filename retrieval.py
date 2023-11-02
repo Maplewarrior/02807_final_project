583577e182e3e2679d8b4b08f72683025d9b985b
MapleWarrior <michaelharborg@gmail.com>
2023-11-02 12:35:40 +0100

    Update retrieval.py

--- retrieval.py
+++ retrieval.py
@@ -23,43 +23,117 @@ class BM25_retriever:
         raise NotImplementedError
 
 class DPR_retriever:
-    def lookup(self, query: str, k: int):
-        """
-        @param query: The input text to which relevant passages should be found.
-        @param k: The number of relevant passages to retrieve.
-        """
-        raise NotImplementedError
-
-    def lookup(self, query: str, k: int):
-        """
-        @param query: The input text to which relevant passages should be found.
-        @param k: The number of relevant passages to retrieve.
-        """
-        raise NotImplementedError
-
-class DPR_rerank_retriever:
     def __init__(self) -> None:
         pass
 
+    def build_index(self, dataset):
+        """
+        @param dataset: The dataset for which an index containing embeddings should be built.
+        """
+        raise NotImplementedError
+    
+    def load_index(self, index_path: str):
+        """
+        @param index_path: The path to the pre-computed index.
+        """
+        raise NotImplementedError
+
+    def embed_query(self, query: str):
+        """
+        @param query: The input text for which relevant passages should be found.
+        returns: An embedding of the query.
+        """
+        raise NotImplementedError
+
     def lookup(self, query: str, k: int):
         """
         @param query: The input text to which relevant passages should be found.
         @param k: The number of relevant passages to retrieve.
         """
         raise NotImplementedError
+
+class DPR_crossencoder_retriever:
+    def __init__(self) -> None:
+        pass
+    
+    def build_index(self, dataset):
+        """
+        @param dataset: The dataset for which an index containing embeddings should be built.
+        """
+        raise NotImplementedError
+    
+    def load_index(self, index_path: str):
+        """
+        @param index_path: The path to the pre-computed index.
+        """
+        raise NotImplementedError
+
+    def embed_query(self, query: str):
+        """
+        @param query: The input text for which relevant passages should be found.
+        returns: An embedding of the query.
+        """
+        raise NotImplementedError
+
+    def lookup(self, query: str, k: int, linkage_function):
+        """
+        @param query: The input text to which relevant passages should be found.
+        @param k: The number of relevant passages to retrieve.
+        @param linkage_function: The function which should 
+        """
+        raise NotImplementedError
+
 class Kmeans_retriever:
     def __init__(self) -> None:
         pass
 
+    def build_index(self, dataset):
+        """
+        @param dataset: The dataset for which an index containing embeddings should be built.
+        """
+        raise NotImplementedError
+    
+    def load_index(self, index_path: str):
+        """
+        @param index_path: The path to the pre-computed index.
+        """
+        raise NotImplementedError
+
+    def embed_query(self, query: str):
+        """
+        @param query: The input text for which relevant passages should be found.
+        returns: An embedding of the query.
+        """
+        raise NotImplementedError
+
     def lookup(self, query: str, k: int):
         """
         @param query: The input text to which relevant passages should be found.
         @param k: The number of relevant passages to retrieve.
         """
         raise NotImplementedError
+
 class CURE_retriever:
     def __init__(self) -> None:
         pass
+    def build_index(self, dataset):
+        """
+        @param dataset: The dataset for which an index containing embeddings should be built.
+        """
+        raise NotImplementedError
+    
+    def load_index(self, index_path: str):
+        """
+        @param index_path: The path to the pre-computed index.
+        """
+        raise NotImplementedError
+
+    def embed_query(self, query: str):
+        """
+        @param query: The input text for which relevant passages should be found.
+        returns: An embedding of the query.
+        """
+        raise NotImplementedError
 
     def lookup(self, query: str, k: int):
         """
