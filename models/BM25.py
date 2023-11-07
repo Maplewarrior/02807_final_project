1183459b726a19ec316606af1d3933450fe4ae0a
Andreas Fiehn <andreas@lyhnefiehn.dk>
2023-11-07 14:42:06 +0100

    Implemented BM25

--- models/BM25.py
+++ models/BM25.py
@@ -1,64 +1,53 @@
 from data.document import Document
 from models.builers.retriever import Retriever
+from models.TFIDF import TFIDF
+import numpy as np
 
 
-class BM25(Retriever):
+class BM25(TFIDF):
     def __init__(self, documents: list[dict] = None, index_path: str = None, k1: float = 1.5, b: float = 0.75) -> None:
         super(BM25, self).__init__(documents, index_path)
+        self.k1 = k1
+        self.b = b
         self.coprus_vocabulary = self.GetCorpusVocabulary()
         self.idf = self.GetInverseDocumentFrequencies()
-        self.tfidf_vectors = self.GetDocumentsTFIDFVectors()
-    
-    def PreprocessText(self, text: str):
-        to_removes = [".",",","?","!",":",";"]
-        for to_remove in to_removes:
-            text = text.replace(to_remove, "")
-        text = text.lower()
-        return text.split(" ")
-    
-    def GetQueryVocabulary(self, query: str):
-        return list(set(self.PreprocessText(query)))
-    
-    def GetDocumentVocabulary(self, document: Document):
-        return list(set(self.PreprocessText(document.GetText())))
-    
-    def GetCorpusVocabulary(self):
-        coprus_vocabulary = set()
-        for document in self.index.GetDocuments():
-            coprus_vocabulary = coprus_vocabulary.union(set(self.PreprocessText(document.GetText())))
-        return coprus_vocabulary
-    
-    def GetDocumentFrequencies(self):
-        term_frequencies = {}
-        for term in self.coprus_vocabulary:
-            term_frequencies[term] = sum(1 for document in self.index.GetDocuments() if term in self.PreprocessText(document.GetText()))
-        return term_frequencies
-    
-    def GetInverseDocumentFrequencies(self):
-        idfs = {}
-        dfs = self.GetDocumentFrequencies()
-        n_documents = len(self.index.GetDocuments())
-        for term, df in dfs.items():
-            idfs[term] = np.log(n_documents / (df + 1))
-        return idfs
-    
-    def GetDocumentsTFIDFVectors(self):
-        tfidf_vectors = []
+        self.bm25_vectorss = self.GetDocumentsBM25Vectors()
+
+
+    def GetAverageDocumentLength(self):
+        return sum([len(self.GetDocumentVocabulary(document)) for document in self.index.GetDocuments()]) / len(self.index.GetDocuments())
+       
+    def GetDocumentsBM25Vectors(self):
+        """ Get the BM25 vectors for all documents in the index.
+        
+        @return: A list of BM25 vectors, where each vector corresponds to a document in the index.
+        """
+        avg_doc_length = self.GetAverageDocumentLength()
+        bm25_vectors = []
         for document in self.index.GetDocuments():
             document_terms = self.GetDocumentVocabulary(document)
-            tfidf_vector = []
+            bm25_vector = []
             for term in self.coprus_vocabulary:
                 tf = document_terms.count(term) / len(document_terms)
-                tfidf = tf * self.idf[term]
-                tfidf_vector.append(tfidf)
-            tfidf_vectors.append(tfidf_vector)
-        return tfidf_vectors
+                # BM25 calculation, see report for details
+                bm25 = self.idf[term] * ((tf * (self.k1 + 1)) / (tf + self.k1 * (1 - self.b + self.b * (len(document_terms) / avg_doc_length))))
+                bm25_vector.append(bm25)
+            bm25_vectors.append(bm25_vector)
+        return bm25_vectors
     
     def CalculateScores(self, query: str):
-        scores = [0 for _ in self.tfidf_vectors]
+        """ Calculate the BM25 scores for all documents in the index.
+        
+        Args:
+            query: The query for which BM25 scores should be calculated.
+        
+        Returns:
+            A list of BM25 scores, where each score corresponds to a document in the index.
+        """
+        scores = [0 for _ in self.bm25_vectorss]
         query_vocabulary = self.GetQueryVocabulary(query)
         for i, term in enumerate(self.coprus_vocabulary):
             if term in query_vocabulary:
-                for j,tfidf_vector in enumerate(self.tfidf_vectors):
-                    scores[j] += tfidf_vector[i]
+                for j,bm25_vectors in enumerate(self.bm25_vectorss):
+                    scores[j] += bm25_vectors[i]
         return scores
